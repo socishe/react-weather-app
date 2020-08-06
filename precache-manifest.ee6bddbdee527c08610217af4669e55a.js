b10bf2c795042228a3ed0457bba967b216af030a
Sihle Socishe <sihlesocishe@gmail.com>
2020-08-06 11:26:34 +0200

    Updates

---
+++ precache-manifest.ee6bddbdee527c08610217af4669e55a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180bfd7eb1f4983e8af0b35f59b710f6",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "de0300eae7d0dac10e29",
+    "url": "/react-weather-app/static/css/main.2f4926f9.chunk.css"
+  },
+  {
+    "revision": "62766a25ba8e4ca9de26",
+    "url": "/react-weather-app/static/js/2.d61ead11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.d61ead11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0300eae7d0dac10e29",
+    "url": "/react-weather-app/static/js/main.7c5b4394.chunk.js"
+  },
+  {
+    "revision": "aacb36d1c4af6b3eab1b",
+    "url": "/react-weather-app/static/js/runtime-main.d770b3fe.js"
+  }
+]);
